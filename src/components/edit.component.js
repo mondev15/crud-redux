354d09dcf23e79b6192a1b4dbc2e550f7cbf6acb
mondev15 <evergiste.mutoni@atos.net>
2019-01-11 12:36:40 +0100

    crud oparation

---
+++ src/components/edit.component.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Edit extends Component {
+    render() {
+        return (
+            <div>
+                <p> welcome to edit component</p>
+            </div>
+        );
+    }
+}
+
+export default Edit;
